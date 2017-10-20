a19dda820a664baa7737d8c1b9b17c548973b21c
Elijah Omolo <eomolo11@gmail.com>
2017-10-19 19:26:26 -0500

    added user validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,18 @@
 class Listing < ApplicationRecord
 mount_uploader :image, ImageUploader
-has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+
+    			#DON"T FORGET TO ADD A COMMA!!
+					#:storage => :aws,
+                    #:dropbox_credentials => Rails.root.join("config/secrets.yml"),
+                    #:path => ":style/:id_:filename"
+end
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  #mount_uploader :image, ImageUploader
+  
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates_attachment_presence :image
 end
