6e367a36653ea3d6041bf3ce327327505d73cf3c
Elijah Omolo <eomolo11@gmail.com>
2017-11-02 10:50:37 -0500

    edited recepient field in listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -39,7 +39,7 @@ class ListingsController < ApplicationController
 
     recipient = Stripe::Account.create(
       :name => current_user.name,
-      :type => "individual",
+      :type => "standar",
       :bank_account => token
       )
 
