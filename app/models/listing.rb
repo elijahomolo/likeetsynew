af3079cec2f61259ff5b453d30a20f0c1fcdc00a
Elijah Omolo <eomolo11@gmail.com>
2017-11-06 13:46:10 -0600

    listings rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,9 +6,9 @@ if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
 
     			#DON"T FORGET TO ADD A COMMA!!
-					:storage => :aws,
-          :dropbox_credentials => Rails.root.join("config/secrets.yml"),
-          :path => ":style/:id_:filename"
+					#:storage => :aws,
+                    #:dropbox_credentials => Rails.root.join("config/secrets.yml"),
+                    #:path => ":style/:id_:filename"
 end
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
